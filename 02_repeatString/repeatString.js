f4a7831877317fd6b26422be07b962e3f4a5f33d
jamesfindlater12 <jamesfindlater11@gmail.com>
2024-11-04 16:31:51 +0000

    update javascript tasks

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,12 @@
-const repeatString = function() {
+const repeatString = function(str, num) {
+    if (num < 0) return "ERROR";
+    let result = ""
+    for (i = 0; i < num; i++) {
+        const input = str;
+        result += input;
+    }
+
+    return result;
 
 };
 
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
-};
+const removeFromArray = function(arr, ...num) {
+        const myArray = [];
+        arr.forEach((item) => {
+            if (!num.includes(item)) {
+                myArray.push(item);
+            }
+        });
+           return myArray; 
+        };
+        
+
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
