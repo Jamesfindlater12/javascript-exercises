cbec68939cda7623b81d58db2af3832efd5e9e1f
jamesfindlater12 <jamesfindlater11@gmail.com>
2024-11-24 21:04:22 +0000

    Update recent exercises

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,18 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    const isLeapYear = year % 4 === 0;
+    const isCenturyYear = year % 100 === 0;
+    const isFourCenturyYear = year % 400 === 0;
 
+    if (isLeapYear &&
+        (isFourCenturyYear || !isCenturyYear)) {
+        return true; 
+    } else {
+        return false;
+    }
+  
 };
+
+console.log(leapYears(1993));
 
 // Do not edit below this line
 module.exports = leapYears;
